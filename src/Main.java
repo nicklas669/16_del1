4292e0e6355ec20b295b352497162072947dbf9b
nicklas669 <nicklas669@gmail.com>
2015-02-11 12:25:28 +0100

    Added some text for the login screen.

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Velkommen til lort");
-		System.out.println("Indtast ID: "); // evt. tjekke om ID er gyldigt
+		
+		int id;
+		String pw;
 		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Velkommen til lort! Du skal logge ind for at komme videre.");
+		System.out.println("Indtast ID: "); // evt. tjekke om ID er gyldigt
+		id = scan.nextInt();
 		System.out.println("Indtast password: ");
+		pw = scan.next();
+		
 	}
 
 }
